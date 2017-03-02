a74c09308001838b1bb6e65dc6609cbc96e9e31b
Jake Weiss <jake@Jakes-MacBook.kidstrap.lan>
2017-03-02 03:04:41 -0500

    more UI i think

--- AppMessage/AppMessage/Controlers/PhotoViewController.swift
+++ AppMessage/AppMessage/Controlers/PhotoViewController.swift
@@ -36,6 +36,7 @@ class PhotoViewController: UIViewController {
     @IBOutlet var yes: UILabel!
     @IBOutlet var no: UILabel!
     @IBOutlet var heading: UILabel!
+    @IBOutlet var backgroundLabel: UILabel!
     
     var image:UIImage!
     var mode:Mode!
@@ -46,27 +47,30 @@ class PhotoViewController: UIViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.bringSubview(toFront: heading)
         cancelButton.isHidden = true
+        backgroundLabel.alpha = 0.0
         if mode == Mode.Sender {
             yes.text = ""
             no.text = ""
             self.gameController.photoViewController = self
+            heading.text = ""
         } else if mode == Mode.ItUser {
             heading.text = "\(gameController.CurrentVote.Sender_Name) Spotted You!"
+            backgroundLabel.alpha = 1.0
             yesButton.isHidden = true
             noButton.isHidden = true
-            yes.text = "Yes:0"
-            no.text = "No:0"
+            yes.text = "Yes: 0"
+            no.text = "No: 0"
+            no.center = CGPoint(x:CGFloat(Float(self.view.frame.width)/4.0), y:noButton.frame.midY)
+            yes.center = CGPoint(x:CGFloat(Float(self.view.frame.width)*3.0/4.0), y:yesButton.frame.midY)
         } else {
             yes.isHidden = true
             no.isHidden = true
             yesButton.setTitle("Confirm", for: UIControlState.normal)
-            yesButton.setTitleColor(UIColor.green, for: UIControlState.normal)
             noButton.setTitle("Deny", for: UIControlState.normal)
-            noButton.titleLabel?.textColor = UIColor.red
-            noButton.setTitleColor(UIColor.red, for: UIControlState.normal)
+            heading.text = ""
         }
-        heading.text = ""
         imageView.image = image
         viewLoadDone = true
     }
@@ -127,6 +131,7 @@ class PhotoViewController: UIViewController {
     @IBAction func save(sender: UIButton) {
         
         if mode == Mode.Sender{
+            backgroundLabel.alpha = 1.0
             heading.text = "Sending..."
             yesButton.isHidden = true
             noButton.isHidden = true
@@ -170,7 +175,7 @@ class PhotoViewController: UIViewController {
                 
                 self.gameController.StartVote(Sender_User_ID: recordIdMe, Sender_Name: name, Asset_ID: record.recordID.recordName)
                 
-                
+                self.backgroundLabel.alpha = 1.0
                 self.heading.text = "Voting in Progress"
                 self.yes.text = "0"
                 self.no.text = "0"
